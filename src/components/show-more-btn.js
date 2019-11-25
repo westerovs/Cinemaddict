842c8f243e10e16c23371df34c90c4021d9077ee
westerovs <westerovs@mail.ru>
2019-11-25 12:59:44 +0300

    25.11.19 kino mission complite

---
+++ src/components/show-more-btn.js
@@ -0,0 +1,4 @@
+// кнопка show-more
+export const createShowMoreTemplate = () => (
+  `<button class="films-list__show-more">Show more</button>`
+);
---
+++ src/components/sort.js
@@ -0,0 +1,8 @@
+// Cортировка
+export const createSortTemplate = () => (
+  `<ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>`
+);
