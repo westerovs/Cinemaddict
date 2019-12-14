4b0367fc76aa07b0bfcd19ceedb5f8271a4f048d
westerovs <westerovs@mail.ru>
2019-12-14 17:14:07 +0300

    новый рендер карточек

--- src/components/film-card-task.js
+++ src/components/film-card-task.js
@@ -17,7 +17,7 @@ const posters = [
 const filmNames = [`Вечное сияние чистого CSS`, `Властелин php`, `Король лвёрстки`, `Достучаться до JS`, `Другие`, `Harry Puthon`, `Joker 2019`, `Спартак`, `Игра престолов`, `Ходячие мертвецы`, `Пила`, `Астрал`, `Бойцовский клуб`, `Большая рыба`, `Titanic`, `Terminator`];
 
 // жарны
-const FILMGENRE = [
+const FILM_GENRES = [
   `Musical`,
   `Triller`,
   `for adults`,
@@ -34,6 +34,7 @@ const descriptionFilm = [
   `Eum non nulla quibusdam rem atque! Quasi quisquam accusantium quas, mollitia pariatur repudiandae in.`,
   `Enim, deleniti, nesciunt.`
 ];
+
 const descriptionFilmSet = new Set([
   randomItem(descriptionFilm),
   randomItem(descriptionFilm),
@@ -52,35 +53,6 @@ function randomNumber(min = 1, max = 999) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-// просмотрен - нет
-function randomWatched() {
-  let random = Boolean(Math.random() >= 0.5);
-  if (random >= 0.5) {
-    return `film-card__controls-item--active`;
-  } else {
-    return ``;
-  }
-}
-
-// фаворит - нет
-function randomFavorite() {
-  let random = Boolean(Math.random() >= 0.5);
-  if (random >= 0.5) {
-    return `film-card__controls-item--active`;
-  } else {
-    return ``;
-  }
-}
-
-// список просмотров - нет
-function randomWatchlist() {
-  let random = Boolean(Math.random() >= 0.5);
-  if (random >= 0.5) {
-    return `film-card__controls-item--active`;
-  } else {
-    return ``;
-  }
-}
 
 // собирает все задачи из массивов в одном объекте
 export const createRandomFilm = () => {
@@ -90,67 +62,40 @@ export const createRandomFilm = () => {
     rating: `${randomNumber(1, 10)} ${randomNumber(1, 10)}`,
     year: randomNumber(1920, 2077),
     time: `${randomNumber(1, 5)}h ${randomNumber(1, 60)}m`,
-    genre: randomItem(FILMGENRE),
+    genre: randomItem(FILM_GENRES),
     description: randomItem([...descriptionFilmSet]),
     comments: randomNumber(),
-    watched: randomWatched(),
-    favorite: randomFavorite(),
-    watchlist: randomWatchlist(),
+    watched: Math.random() > 0.5,
+    favorite: Math.random() > 0.5,
+    watchlist: Math.random() > 0.5,
   };
 };
 
-
-// ********************************************************
-// ********************************************************
 // ******************** шаблон фильма *********************
-export const createFilmCardkTemplate = function () {
-  // filmRandom - вызывает объект задач из createRandomFilm
-  const filmRandom = createRandomFilm();
+export const createFilmCardTemplate = function (film) {
+  // createRandomFilmMarkup - вызывает объект задач из createRandomFilm
+  const {poster, name, rating, year, time, genre, description, comments, watched, favorite, watchlist} = film;
 
   return (
     `<article class="film-card">
-    <h3 class="film-card__title">${filmRandom.name}</h3>
-    <p class="film-card__rating">${filmRandom.rating}</p>
-    <p class="film-card__info">
-      <span class="film-card__year">${filmRandom.year}</span>
-      <span class="film-card__duration">${filmRandom.time}</span>
-      <span class="film-card__genre">${filmRandom.genre}</span>
-    </p>
-    <img src="./images/posters/${filmRandom.poster}" alt="" class="film-card__poster">
-    <p class="film-card__description">
-      ${filmRandom.description}
-    </p>
-    <a class="film-card__comments">${filmRandom.comments} comments</a>
-    <form class="film-card__controls">
-      <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${filmRandom.watchlist}">Add to watchlist</button>
-      <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${filmRandom.watched}">Mark as watched</button>
-      <button class="film-card__controls-item button film-card__controls-item--favorite ${filmRandom.favorite}">Mark as favorite</button>
-    </form>
+    <h3 class="film-card__title">${name}</h3>
+       <p class="film-card__rating">${rating}</p>
+       <p class="film-card__info">
+         <span class="film-card__year">${year}</span>
+         <span class="film-card__duration">${time}</span>
+         <span class="film-card__genre">${genre}</span>
+       </p>
+       <img src="./images/posters/${poster}" alt="" class="film-card__poster">
+       <p class="film-card__description">
+        ${description}
+       </p>
+       <a class="film-card__comments">${comments} comments</a>
+       <form class="film-card__controls">
+         <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist${watchlist}">Add to watchlist</button>
+         <button class="film-card__controls-item button film-card__controls-item--mark-as-watched${watched}">Mark as watched</button>
+         <button class="film-card__controls-item button film-card__controls-item--favorite${favorite}">Mark as favorite</button>
+       </form>
     </article>`
   );
 };
 
-
-/*
-абстрактно фильм для тебя - это объект с полями, которые я перечислил.
-Вот у тебя функция должна возвращать объект с такими полями. И каждое поле заполняется случайно, с помощью функции getRandom и массива
-
-что у меня сейчас и происходит
-массив попадает в генератор и фильм принимает данные из генератора... так ?
-
-- Не совсем. Ты возвращаешь массив. А надо один фильм Объектом
-
-- Вот и напиши функцию, которая возвращает такой объект. Только назови нормально. Название функции это глагол.
-
-- Как generateTask, также ты должен написать generateFilm
-
-она в себя около 5 функций принимает каждая функция в себя по массиву принимает
-так ?
-
-Не принимает, а внутри себя вызывает Генерируя случайно каждое поле
-
-Вот, ты вызываешь функцию, и зачем то передаешь параметр year, хотя она не принимает параметры
-К тому же переменной year нет в коде, поэтому ошибка
-const film = createRandomFilm ();
-console.log(film.year);
-*/
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {createProfileTemplate} from './components/profile.js';
 import {createNavigationTemplate} from './components/navigation.js';
 import {createSortTemplate} from './components/sort.js';
 import {createFilmsListTemplate} from './components/film-list.js';
-import {createFilmCardkTemplate} from './components/film-card-task.js';
+import {createRandomFilm, createFilmCardTemplate} from './components/film-card-task.js';
 import {createBtnShowMoreTemplate} from './components/btn-show-more.js';
 // import {generateNavItems} from './mock/navigation-mock.js';
 // поп-арт закоментировал, что бы не закрывал страницу
@@ -35,20 +35,31 @@ render(siteMainElement, createSortTemplate());
 render(siteMainElement, createFilmsListTemplate());
 
 
-// контейнер фильмов верхний
+// *************************************************************
+// *************************************************************
 const taskListElement = siteMainElement.querySelector(`.films-list__container`);
-// const filmEnter = createRandomFilm();
-function filmRender() {
-  new Array(TASK_COUNT).fill(``).forEach(() => render(taskListElement, createFilmCardkTemplate()));
-  return TASK_COUNT;
-}
-filmRender(TASK_COUNT);
+const count = 10;
+
+
+// функция рендера случайных фильмов с заданным количеством
+const createRandomFilmsMarkup = (count) => {
+  // HTML разметка созданная из случайных фильмов
+  return new Array(count)
+    .fill(``)
+    .map(() => {
+      const film = createRandomFilm();
+      return createFilmCardTemplate(film);
+    })
+    .join(``);
+};
+
+render(taskListElement, createRandomFilmsMarkup(TASK_COUNT));
 
 
 // блоки экстра фильмы
 const extraListElements = siteMainElement.querySelectorAll(`.films-list--extra .films-list__container`);
 for (let i = 0; i < extraListElements.length; i++) {
-  new Array(2).fill(``).forEach(() => render(extraListElements[i], createFilmCardkTemplate()));
+  render(extraListElements[i], createRandomFilmsMarkup(2));
 }
 
 
@@ -57,15 +68,27 @@ const boardElement = siteMainElement.querySelector(`.films-list`);
 render(boardElement, createBtnShowMoreTemplate());
 const btnLoad = siteMainElement.querySelector(`.films-list__show-more`);
 
+
 // ф-ция добавить ещё карточек и удалить кнопку
 btnLoad.onclick = () => {
-  filmRender(5);
+  render(taskListElement, createRandomFilmsMarkup(5));
   // карточка фильма
   let taskFilmCard = siteMainElement.querySelectorAll(`.films-list .film-card`);
-  if (taskFilmCard.length === 20) {
+  if (taskFilmCard.length >= 20) {
     btnLoad.style.display = `none`;
   }
 };
+
+
+// ф-ция добавить ещё карточек и удалить кнопку
+// btnLoad.onclick = () => {
+//   filmRender(5);
+//   // карточка фильма
+//   let taskFilmCard = siteMainElement.querySelectorAll(`.films-list .film-card`);
+//   if (taskFilmCard.length === 20) {
+//     btnLoad.style.display = `none`;
+//   }
+// };
 
 
 // поп-арт - закоментировал что бы не закрывал проект
