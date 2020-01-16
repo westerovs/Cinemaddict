1e21c7da2775f1a842955c0eba084fba53a3c1ee
westerovs <westerovs@mail.ru>
2020-01-16 21:26:50 +0300

    правки package

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const path = require(`path`);
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 
 module.exports = {
-  mode: `development`, // Режим сборки
-  entry: `./src/main.js`, // Точка входа приложения
-  output: { // Настройка точка выхода
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`),
+    path: path.join(__dirname, `public`)
   },
-  devtool: `source-map`, // подключаем sourcemaps
+  devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, `public`), // Где искать сборку
-    compress: true, // Сжатие
-    // Автоматическая перезагрузка страницы
-    // Если не работает по стандартному URLу в браузере ‘http://localhost:8080’,
-    // то добавьте к нему ‘/webpack-dev-server/‘: ‘http://localhost:8080/webpack-dev-server/'
-    watchContentBase: true,
-  }
+    contentBase: path.join(__dirname, `public`),
+    compress: true
+  },
+  plugins: [
+    new MomentLocalesPlugin({
+      localesToKeep: [`es-us`],
+    })
+  ]
 };
