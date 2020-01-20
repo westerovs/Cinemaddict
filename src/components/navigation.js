254c4415a4584fdc2829d24870eb6384d1d1b8a2
westerovs <westerovs@mail.ru>
2020-01-20 16:59:06 +0300

    fix

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,12 +7,6 @@ const createNavigationTemplate = function () {
       <a href="#all" class="main-navigation__item main-navigation__item--active">
         All movies
       </a>
-      <a href="#watchlist" class="main-navigation__item" id="sort-up">
-        По возрастанию
-      </a>
-      <a href="#watchlist" class="main-navigation__item" id="sort-down">
-        По убыванию
-      </a>
       <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">999</span></a>
       <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">999</span></a>
       <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">999</span></a>
