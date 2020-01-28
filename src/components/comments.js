ac71e1f1c1c2dc4569e288ef72ace48f298ddafb
westerovs <westerovs@mail.ru>
2020-01-28 11:09:40 +0300

    мелкие правки

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,12 +1,68 @@
 import AbstractComponent from './abstract-component.js';
 
-const commentsTemplate = (popComm) => {
-  const {comments} = popComm;
+
+const commentsTemplate = (comment) => {
+  const {commentsCount} = comment;
+
   return (
     `<section class="film-details__comments-wrap">
-      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments}</span></h3>
+      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${commentsCount}</span></h3>
 
-      <ul class="film-details__comments-list"></ul>
+        <ul class="film-details__comments-list">
+
+        <li class="film-details__comment">
+          <span class="film-details__comment-emoji">
+            <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji">
+          </span>
+          <div>
+            <p class="film-details__comment-text">Interesting setting and a good cast</p>
+            <p class="film-details__comment-info">
+              <span class="film-details__comment-author">Tim Macoveev</span>
+              <span class="film-details__comment-day">2019/12/31 23:59</span>
+              <button class="film-details__comment-delete">Delete</button>
+            </p>
+          </div>
+        </li>
+        <li class="film-details__comment">
+          <span class="film-details__comment-emoji">
+            <img src="./images/emoji/sleeping.png" width="55" height="55" alt="emoji">
+          </span>
+          <div>
+            <p class="film-details__comment-text">Booooooooooring</p>
+            <p class="film-details__comment-info">
+              <span class="film-details__comment-author">John Doe</span>
+              <span class="film-details__comment-day">2 days ago</span>
+              <button class="film-details__comment-delete">Delete</button>
+            </p>
+          </div>
+        </li>
+        <li class="film-details__comment">
+          <span class="film-details__comment-emoji">
+            <img src="./images/emoji/puke.png" width="55" height="55" alt="emoji">
+          </span>
+          <div>
+            <p class="film-details__comment-text">Very very old. Meh</p>
+            <p class="film-details__comment-info">
+              <span class="film-details__comment-author">John Doe</span>
+              <span class="film-details__comment-day">2 days ago</span>
+              <button class="film-details__comment-delete">Delete</button>
+            </p>
+          </div>
+        </li>
+        <li class="film-details__comment">
+          <span class="film-details__comment-emoji">
+            <img src="./images/emoji/angry.png" width="55" height="55" alt="emoji">
+          </span>
+          <div>
+            <p class="film-details__comment-text">Almost two hours? Seriously?</p>
+            <p class="film-details__comment-info">
+              <span class="film-details__comment-author">John Doe</span>
+              <span class="film-details__comment-day">Today</span>
+              <button class="film-details__comment-delete">Delete</button>
+            </p>
+          </div>
+        </li>
+        </ul>
 
       <div class="film-details__new-comment">
         <div for="add-emoji" class="film-details__add-emoji-label"></div>
@@ -43,13 +99,13 @@ const commentsTemplate = (popComm) => {
 
 
 export default class Comments extends AbstractComponent {
-  constructor(popComm) {
+  constructor(comment) {
     super();
-    this._popComm = popComm;
+    this._comment = comment;
   }
 
   getTemplate() {
-    return commentsTemplate(this._popComm);
+    return commentsTemplate(this._comment);
   }
 }
 
